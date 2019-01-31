03d72cafdbac720b16d56ade46ab6d342e63945a
jason <jason@JASON-PC>
2019-01-31 17:16:07 +0800

    add cache

---
+++ src/MS/Runtime/Caching/Memory/MSMemoryCache.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.Extensions.Options;
+using Microsoft.Extensions.Caching.Memory;
+using MS.Runtime.Caching;
+using MS;
+
+namespace Abp.Runtime.Caching.Memory
+{
+    /// <summary>
+    /// Implements <see cref="ICache"/> to work with <see cref="MemoryCache"/>.
+    /// </summary>
+    public class MSMemoryCache : CacheBase
+    {
+        private MemoryCache _memoryCache;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="name">Unique name of the cache</param>
+        public MSMemoryCache(string name)
+            : base(name)
+        {
+            _memoryCache = new MemoryCache(new OptionsWrapper<MemoryCacheOptions>(new MemoryCacheOptions()));
+        }
+
+        public override object GetOrDefault(string key)
+        {
+            return _memoryCache.Get(key);
+        }
+
+        public override object Get(string key, Func<string, object> factory)
+        {
+            return base.Get(key, factory);
+        }
+
+        public override void Set(string key, object value, TimeSpan? slidingExpireTime = null, TimeSpan? absoluteExpireTime = null)
+        {
+            if (value == null)
+            {
+                throw new MSException("Can not insert null values to the cache!");
+            }
+
+            if (absoluteExpireTime != null)
+            {
+                _memoryCache.Set(key, value, DateTimeOffset.Now.Add(absoluteExpireTime.Value));
+            }
+            else if (slidingExpireTime != null)
+            {
+                _memoryCache.Set(key, value, slidingExpireTime.Value);
+            }
+            else if (DefaultAbsoluteExpireTime != null)
+            {
+                _memoryCache.Set(key, value, DateTimeOffset.Now.Add(DefaultAbsoluteExpireTime.Value));
+            }
+            else
+            {
+                _memoryCache.Set(key, value, DefaultSlidingExpireTime);
+            }
+        }
+
+        public override void Remove(string key)
+        {
+            _memoryCache.Remove(key);
+        }
+
+        public override void Clear()
+        {
+            _memoryCache.Dispose();
+            _memoryCache = new MemoryCache(new OptionsWrapper<MemoryCacheOptions>(new MemoryCacheOptions()));
+        }
+
+        public override void Dispose()
+        {
+            _memoryCache.Dispose();
+            base.Dispose();
+        }
+    }
+}
---
+++ src/MS/Runtime/Caching/Memory/MSMemoryCacheManager.cs
@@ -0,0 +1,39 @@
+﻿using MS.Dependency;
+using Castle.Core.Logging;
+using MS.Runtime.Caching;
+
+namespace Abp.Runtime.Caching.Memory
+{
+    /// <summary>
+    /// Implements <see cref="ICacheManager"/> to work with MemoryCache.
+    /// </summary>
+    public class MSMemoryCacheManager : CacheManagerBase
+    {
+        public ILogger Logger { get; set; }
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public MSMemoryCacheManager(IIocManager iocManager)
+            : base(iocManager)
+        {
+            Logger = NullLogger.Instance;
+        }
+
+        protected override ICache CreateCacheImplementation(string name)
+        {
+            return new MSMemoryCache(name)
+            {
+                Logger = Logger
+            };
+        }
+
+        protected override void DisposeCaches()
+        {
+            foreach (var cache in Caches.Values)
+            {
+                cache.Dispose();
+            }
+        }
+    }
+}
