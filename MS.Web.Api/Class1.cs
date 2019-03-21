2aeeb1bc5a89157f9f97c0ed0ee50a27b8fa0282
jason <jason@JASON-PC>
2019-03-21 14:38:11 +0800

    add dynamic api project

---
+++ MS.Web.Api/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MS.Web.Api
+{
+    public class Class1
+    {
+    }
+}
---
+++ MS.Web.Api/MSWebApiModule.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MS.Web.Api
+{
+    class MSWebApiModule
+    {
+    }
+}
