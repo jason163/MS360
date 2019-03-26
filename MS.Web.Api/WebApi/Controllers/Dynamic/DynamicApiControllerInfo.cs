90674e789db34ac257adb378e92d3605ab0ec984
jason <jason@JASON-PC>
2019-03-26 10:06:43 +0800

    add

--- MS.Web.Api/WebApi/Controllers/Dynamic/DynamicApiControllerInfo.cs
+++ MS.Web.Api/WebApi/Controllers/Dynamic/DynamicApiControllerInfo.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Http.Filters;
 
 namespace MS.WebApi.Controllers.Dynamic
 {
@@ -11,5 +12,67 @@ namespace MS.WebApi.Controllers.Dynamic
     /// </summary>
     public class DynamicApiControllerInfo
     {
+        /// <summary>
+        /// 服务名称
+        /// </summary>
+        public string ServiceName { get; private set; }
+
+        /// <summary>
+        /// 服务接口类型
+        /// </summary>
+        public Type ServiceInterfaceType { get; private set; }
+
+        /// <summary>
+        /// Controller 类型
+        /// </summary>
+        public Type ApiControllerType { get; private set; }
+
+        /// <summary>
+        /// 接口类型
+        /// </summary>
+        public Type InterceptorType { get; private set; }
+
+        /// <summary>
+        /// Is API Explorer enabled.
+        /// </summary>
+        public bool? IsApiExplorerEnabled { get; private set; }
+
+        /// <summary>
+        /// 过滤器列表
+        /// </summary>
+        public IFilter[] Filters { get; set; }
+
+        /// <summary>
+        /// Action列表
+        /// </summary>
+        public IDictionary<string, DynamicApiActionInfo> Actions { get; private set; }
+        
+
+        /// <summary>
+        /// Creates a new <see cref="DynamicApiControllerInfo"/> instance.
+        /// </summary>
+        /// <param name="serviceName">Name of the service</param>
+        /// <param name="serviceInterfaceType">Service interface type</param>
+        /// <param name="apiControllerType">Api Controller type</param>
+        /// <param name="interceptorType">Interceptor type</param>
+        /// <param name="filters">Filters</param>
+        /// <param name="isApiExplorerEnabled">Is API explorer enabled</param>
+        public DynamicApiControllerInfo(
+            string serviceName,
+            Type serviceInterfaceType,
+            Type apiControllerType,
+            Type interceptorType,
+            IFilter[] filters = null,
+            bool? isApiExplorerEnabled = null)
+        {
+            ServiceName = serviceName;
+            ServiceInterfaceType = serviceInterfaceType;
+            ApiControllerType = apiControllerType;
+            InterceptorType = interceptorType;
+            IsApiExplorerEnabled = isApiExplorerEnabled;
+            Filters = filters ?? new IFilter[] { };
+
+            Actions = new Dictionary<string, DynamicApiActionInfo>(StringComparer.InvariantCultureIgnoreCase);
+        }
     }
 }
---
+++ MS.Web.Api/WebApi/Controllers/Dynamic/DynamicApiControllerManager.cs
@@ -0,0 +1,39 @@
+﻿using MS.Dependency;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MS.Extension;
+using System.Collections.Immutable;
+
+namespace MS.WebApi.Controllers.Dynamic
+{
+    /// <summary>
+    /// 存储管理动态API对象信息
+    /// </summary>
+    public class DynamicApiControllerManager : ISingletonDependency
+    {
+        private readonly IDictionary<string, DynamicApiControllerInfo> _dynamicApiControllers;
+
+        public DynamicApiControllerManager()
+        {
+            _dynamicApiControllers = new Dictionary<string, DynamicApiControllerInfo>();
+        }
+
+        public void Register(DynamicApiControllerInfo controllerInfo)
+        {
+            _dynamicApiControllers[controllerInfo.ServiceName] = controllerInfo;
+        }
+
+        public DynamicApiControllerInfo FindOrNull(string controllerName)
+        {
+            return _dynamicApiControllers.GetOrDefault(controllerName);
+        }
+
+        public IReadOnlyList<DynamicApiControllerInfo> GetAll()
+        {
+            return _dynamicApiControllers.Values.ToImmutableList();
+        }
+    }
+}
