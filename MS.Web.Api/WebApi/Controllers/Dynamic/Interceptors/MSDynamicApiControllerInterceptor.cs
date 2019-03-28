cb040032feb0ab0d15bd5da142cc13d6ba556c20
jason <jason@JASON-PC>
2019-03-28 11:47:31 +0800

    add

---
+++ MS.Web.Api/WebApi/Controllers/Dynamic/Interceptors/MSDynamicApiControllerInterceptor.cs
@@ -0,0 +1,52 @@
+﻿using Castle.DynamicProxy;
+using MS.WebApi.Controllers.Dynamic.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MS.WebApi.Controllers.Dynamic.Interceptors
+{
+    /// <summary>
+    /// 动态Api 拦截器,会拦截所有action的调用
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class MSDynamicApiControllerInterceptor<T> : IInterceptor
+    {
+        /// <summary>
+        /// 被代理的真实对象实例
+        /// </summary>
+        private readonly T _proxiedObject;
+
+        public MSDynamicApiControllerInterceptor(T proxiedObject)
+        {
+            _proxiedObject = proxiedObject;
+        }
+
+        public void Intercept(IInvocation invocation)
+        {
+            if (DynamicApiControllerActionHelper.IsMethodOfType(invocation.Method, typeof(T)))
+            {
+                try
+                {
+                    invocation.ReturnValue = invocation.Method.Invoke(_proxiedObject, invocation.Arguments);
+                }catch(TargetInvocationException targetInvocation)
+                {
+                    if(targetInvocation.InnerException != null)
+                    {
+                        ExceptionDispatchInfo.Capture(targetInvocation.InnerException).Throw();
+                    }
+                    throw;
+                }
+            }
+            else
+            {
+                // 正常调用api controller的方法
+                invocation.Proceed();
+            }
+        }
+    }
+}
---
+++ src/MS/Application/Services/ApplicationService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MS.Application.Services
+{
+    public abstract class ApplicationService : IApplicationService
+    {
+    }
+}
