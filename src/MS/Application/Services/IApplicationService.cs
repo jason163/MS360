eaf75d096b22479af316e432c9871ca99d408d5e
jason <jason@JASON-PC>
2019-03-28 11:22:40 +0800

    add

---
+++ src/MS/Application/Services/IApplicationService.cs
@@ -0,0 +1,14 @@
+﻿using MS.Dependency;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MS.Application.Services
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IApplicationService : ITransientDependency
+    {
+    }
+}
