f6a7b1d3594c7470eaa68b18bb8da0096ea8c584
jason <jason@JASON-PC>
2019-04-11 11:56:14 +0800

    add application model conventions

---
+++ src/MS.AspNetCore/AspNetCore/Mvc/Conventions/MSAppServiceConvention.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MS.AspNetCore.Mvc.Conventions
+{
+    /// <summary>
+    /// 自定义ApplicationModel
+    /// </summary>
+    public class MSAppServiceConvention : IApplicationModelConvention
+    {
+        public void Apply(ApplicationModel application)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
